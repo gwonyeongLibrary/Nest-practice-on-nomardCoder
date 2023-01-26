19349fba4b9eff6ba8f7242955ffeb00b458c4d7
Gwonyeong <rnjsdud980@naver.com>
2023-01-26 22:36:04 +0900

    Add: unit test

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ nest는 express 위에서 돌아가기 때문에
 getAll(@Req() req, @Res() res) 같은 식으로 express에서 사용하는 방법을 똑같이 사용할 수 있음.
 
 Nest는 express와 fastify 두개의 프레임 워크 위에서 돌아간다.
+
+---
+
+## Test
+
+- 유닛 테스트
+  - 함수 하나를 테스트 하기
+- e2e 테스트 (End to end)
+  - 사용자 관점에서 시나리오를 작성
+  - 전체적인 서비스를 테스트 하는 것.
--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,11 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  //테스트를 하기 전 실행
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -11,8 +13,70 @@ describe('MoviesService', () => {
 
     service = module.get<MoviesService>(MoviesService);
   });
-
+  // individual test
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        gerners: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual('Movie with ID 999: Not Fount');
+      }
+    });
+    describe('deleteOne', () => {
+      it('deletes a movie', () => {
+        service.create({
+          title: 'Test Movie',
+          gerners: ['test'],
+          year: 2000,
+        });
+        const allMovies = service.getAll();
+        service.deleteOne(1);
+        const afterDelete = service.getAll();
+        expect(afterDelete.length).toEqual(allMovies.length - 1);
+      });
+      it('should return a 404', () => {
+        try {
+          service.deleteOne(999);
+        } catch (err) {
+          expect(err).toBeInstanceOf(NotFoundException);
+        }
+      });
+    });
+    describe('create', () => {
+      it('should create a movie', () => {
+        const beforeCreate = service.getAll().length;
+        service.create({
+          title: 'Test Movie',
+          gerners: ['test'],
+          year: 2000,
+        });
+        const afterCreate = service.getAll().length;
+        console.log(beforeCreate, afterCreate);
+        expect(afterCreate).toBeGreaterThan(beforeCreate);
+      });
+    });
   });
 });
