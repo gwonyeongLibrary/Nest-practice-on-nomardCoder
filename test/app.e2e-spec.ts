36ed77b3b644da5f64d553784deba7ae21cb0b05
Gwonyeong <rnjsdud980@naver.com>
2023-01-28 22:27:45 +0900

    Add: Jest test Code

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true, // 유저가 보낸 타입을 원하는 타입으로 변환해준다.
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +26,32 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my api');
+  });
+  describe('/movies', () => {
+    it('/movies (GET)', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'TEST',
+          year: 2000,
+          gerners: ['test'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('movies/:id', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies/2').expect(404);
+    });
   });
 });
